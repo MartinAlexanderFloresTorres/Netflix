87ec650319ac142ca975ee586f9256e243f3774c
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-02-27 14:15:04 -0500

    primer sitio web de neflix

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/////////////////// main-toogle ///////////////////
+const main_toogle = document.querySelectorAll('.main-toogle');
+
+main_toogle.forEach((elemento) => {
+    elemento.addEventListener('click', ()=>{
+        elemento.classList.toggle('active');
+    });
+});
+
+
+const fondo = document.querySelector('.header');
+
+let aumento = 1;
+const cambiarFondo = (e) =>{
+
+    if(aumento == 1){
+        fondo.style.backgroundImage =  "url(img/banner.jpg)";
+    }
+
+    if(aumento == 2){
+        fondo.style.backgroundImage = "url(img/6.jpg)";
+    }
+
+    if(aumento == 3){
+        fondo.style.backgroundImage = "url(img/3.jpeg)";
+    }
+
+    if(aumento == 4){
+        fondo.style.backgroundImage = "url(img/4.jpg)";
+    }
+    if(aumento == 5){
+        fondo.style.backgroundImage = "url(img/5.jpeg)";
+    }
+    if(aumento == 6){
+        fondo.style.backgroundImage = "url(img/2.jpg)";
+ }
+    if(aumento >= 7){
+        aumento = 0;
+    }
+}
+setInterval(() => {
+    cambiarFondo();
+    aumento++;
+},3000);
