d0cdd0f9cd8f74aa3d184e67e4aa11ddef058ff6
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-04-13 10:43:38 -0500

    validacion de formulario

--- js/main.js
+++ js/main.js
@@ -86,3 +86,73 @@ body.addEventListener("click", (e) => {
         ejecutarCambio();
     }
 });
+
+//===== validad formulario 1 =====
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const formulario = document.querySelector("#formulario-uno");
+const emailUno = document.querySelector("#email-uno");
+const invalidoUno = document.querySelector(".invalido");
+const botonUno = document.querySelector("#boton-uno");
+const spinerUno = document.querySelector("#spinerUno");
+
+//===== validar formulario 2 =====
+const formularioDos = document.querySelector("#formulario-dos");
+const emailDos = document.querySelector("#email-dos");
+const invalidoDos = document.querySelector(".invalido-dos");
+const botonDos = document.querySelector("#boton-dos");
+const spinerDos = document.querySelector("#spinerDos");
+
+formulario.addEventListener("submit", validarFormulario);
+formularioDos.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+    e.preventDefault();
+    if (e.target.id == "formulario-uno") {
+        validar(formulario, emailUno, botonUno, invalidoUno, spinerUno);
+    }
+    if (e.target.id == "formulario-dos") {
+        validar(formularioDos, emailDos, botonDos, invalidoDos, spinerDos);
+    }
+}
+function validar(formulario, email, boton, mensaje, spiner) {
+    if (er.test(email.value)) {
+        mensaje.style.top = "50%";
+        enviarFormulario(formulario, email, boton, mensaje, spiner);
+    } else {
+        mensaje.style.top = "-50%";
+        setTimeout(() => {
+            mensaje.style.top = "50%";
+        }, 2000);
+    }
+}
+let url = location.href;
+function enviarFormulario(formulario, email, boton, mensaje, spiner) {
+    email.style.display = "none";
+    boton.style.display = "none";
+    mensaje.style.display = "none";
+    spiner.style.display = "block";
+    setTimeout(() => {
+        spiner.style.display = "none";
+        mensaje.classList.remove("invalido");
+        mensaje.classList.add("valido");
+        mensaje.style.display = "block";
+        if (location.href == url & url.includes("index.html")) {
+            mensaje.textContent = "Datos Enviados Correctamente"
+        } else{
+            mensaje.textContent = "Data Sent Successfully"
+        }
+        setTimeout(() => {
+            email.style.display = "block";
+            boton.style.display = "block";
+            mensaje.style.display = "block";
+            mensaje.classList.add("invalido");
+            mensaje.classList.remove("valido");
+            if (location.href == url & url.includes("index.html")) {
+                mensaje.textContent = "Email Invalido";
+            } else{
+                mensaje.textContent = "Invalid Email";
+            }
+            formulario.reset();
+        }, 2500);
+    }, 2000);
+}
